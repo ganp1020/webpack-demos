c16daaab78fede16a2b15e288983abeeb704bf6d
ganping <ganp1020@126.com>
2020-04-17 11:19:58 +0800

    feat：配置多页面

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: {//入口
-        index:'./src/index.js',
-        login:'./src/login.js',
+        index: './src/index.js',
+        login: './src/login.js',
     },
     output: { // 出口
         filename: "[name].js",
@@ -15,7 +15,15 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html", // // 在src目录下创建一个index.html页面当做模板来用
-            hash: true//// 会在打包好的bundle.js后面加上hash串
+            filename: "index.html",
+            hash: true,// 会在打包好的bundle.js后面加上hash串
+            chunks: ['index']// 对应js文件
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/login.html", // // 在src目录下创建一个index.html页面当做模板来用
+            filename: "login.html",
+            hash: true,//// 会在打包好的bundle.js后面加上hash串
+            chunks: ['login']
         })
     ],  // 插件配置
     devServer: {   // 开发服务器配置
